b13188db370e8afc6c9e8ee76040f0445b31b48b
matej-marek <72819092+matej-marek@users.noreply.github.com>
2020-12-26 20:03:51 +0100

    add layers showing output

--- src/canvasLayering.js
+++ src/canvasLayering.js
@@ -5,8 +5,6 @@ class Layer{
         this.whole=whole;
         this.position=position||[0,0];
         this.whole.idCounter++;
-        this.layerData=[];
-        this.layerImage=[];
         this.id=this.whole.idCounter;
         this.positionLayer=this.whole.layers.length;
         this.canvas=document.createElement("canvas");
@@ -19,7 +17,8 @@ class Layer{
         this.alpha=[];
         this.jas=0;
         this.alphaProcesingData=100;
-        this.imageData=this.context.getImageData(0,0,this.size[0],this.size[1]);
+        this.layerImage=this.context.getImageData(0,0,this.size[0],this.size[1]);
+        this.layerData=this.layerImage;
     }
     clear(){
         this.context.clearRect(0, 0, this.size[0], this.size[1])
@@ -33,6 +32,7 @@ class Layer{
         setTimeout(function(){ 
         that.context.drawImage(img,0,0,that.size[0],that.size[1]);
         that.whole.render(); 
+        that.ImageDataStarted();
         }, 200);
         
     }
@@ -121,7 +121,7 @@ class Layer{
               data[x]=255;
             }
         }
-        this.YCbCrtoRGB()
+        this.YCbCrtoRGB();
     }
     alphaProcesing(procent){
         if(procent<0){
@@ -145,7 +145,7 @@ class Layer{
     }
     
     ImageDataStarted(){
-        this.layerImage=this.layer.scene.context.getImageData(0,0,this.size[0],this.size[1]);
+        this.layerImage=this.context.getImageData(0,0,this.size[0],this.size[1]);
         this.layerData=this.layerImage.data;
         this.rgb=[];
         this.alpha=[]
@@ -170,7 +170,8 @@ class Layer{
             this.layerData[i+3+plus]=this.alpha[plus];
             plus++;
         }
-        this.layer.scene.context.putImageData(this.layerImage,0,0);
+        this.context.putImageData(this.layerImage,0,0);
+        this.whole.render()
     }
 }
 class Canvas{
@@ -284,7 +285,7 @@ class Canvas{
                 datas.push([i,0]); 
             }
             var data = google.visualization.arrayToDataTable(datas);
-            var divHist='<div class="histograms"><div class="btns-hist"><button onclick="changeHistColor(this)" class="canvasLayeringBtnHist cLBHActive" data-type="y"></button><button  onclick="changeHistColor(this)"  class="canvasLayeringBtnHist" data-type="r"></button><button onclick="changeHistColor(this)"  class="canvasLayeringBtnHist" data-type="g"></button><button onclick="changeHistColor(this)" class="canvasLayeringBtnHist" data-type="b"></button></div><div id="CLHistogram"></div></div>';
+            var divHist='<div class="histograms"><div class="btns-hist"><button onclick="changeHistColor(this)" class="canvasLayeringBtnHist cLBHActive" data-type="y">Y</button><button  onclick="changeHistColor(this)"  class="canvasLayeringBtnHist" data-type="r">R</button><button onclick="changeHistColor(this)"  class="canvasLayeringBtnHist" data-type="g">G</button><button onclick="changeHistColor(this)" class="canvasLayeringBtnHist" data-type="b">B</button></div><div id="CLHistogram"></div></div>';
             document.getElementById(div).innerHTML+=divHist;
             setTimeout(function(){
             that.chart = new google.visualization.AreaChart(document.getElementById("CLHistogram"));
@@ -292,7 +293,6 @@ class Canvas{
         }
     }
     updateHistogram(){
-        console.log(this.histView);
         if(this.histView){
             var type= document.getElementsByClassName("cLBHActive")[0].getAttribute("data-type")||"y";
             var data ="";
@@ -323,10 +323,92 @@ class Canvas{
             this.chart.draw(arr, this.options);
         }
     }
+    layershow(divLayers){
+        var layers=document.getElementById(divLayers);
+        var activeLayer=document.getElementsByClassName("activeLayer").length;
+        if(activeLayer>0){
+            var hodnotaActiveLayer=document.getElementsByClassName("activeLayer")[0].getAttribute("layer-id");
+        }
+        else{
+            var hodnotaActiveLayer=0;
+        }
+        layers.innerHTML="";
+        var x=0;
+        for(var i=this.layers.length-1;i>=0;i--){
+            if(i==this.layers.length-1){
+                var data="<input type='text' class='layerName' layer-id='"+i+"' value='"+this.layers[i].name+"'><button class='layerDown' layer-id='"+i+"'>↓</button>";
+            }
+            else if(i==0){
+                var data="<input type='text' class='layerName' layer-id='"+i+"' value='"+this.layers[i].name+"'><button class='layerUp' layer-id='"+i+"'>↑</button>";
+            }
+            else{ 
+                var data="<input type='text' class='layerName' layer-id='"+i+"' value='"+this.layers[i].name+"'><button class='layerUp' layer-id='"+i+"'>↑</button><button class='layerDown' layer-id='"+i+"'>↓</button>";
+            }
+            var img=imagedata_to_image(this.layers[i].layerImage,i); 
+            if(i==this.layers.length-1){
+                layers.innerHTML+="<div id='layerShowing"+i+"' class='layerShowing activeLayer' layer-id='"+i+"'>"+data+"<button onclick='deleteLayer("+i+")'>X</button></div>";    
+            }
+            else{
+                if(activeLayer>0&&hodnotaActiveLayer==i){
+                    layers.innerHTML+="<div id='layerShowing"+i+"' class='layerShowing activeLayer' layer-id='"+i+"'>"+data+"<button onclick='deleteLayer("+i+")'>X</button></div>";    
+                }
+                else{
+                    layers.innerHTML+="<div id='layerShowing"+i+"' class='layerShowing' layer-id='"+i+"'>"+data+"<button onclick='deleteLayer("+i+")'>X</button></div>";    
+                }
+            }  
+            var div = document.getElementById("layerShowing"+i);
+            if(div==null){
+                div = document.createElement("div");
+                div.id="layerShowing"+i;
+            }
+            console.log(div);
+            var oldData = div.innerHTML;
+            div.innerHTML="";
+            if(this.layers[i].visible){
+                div.innerHTML+="<button class='layerVisibility' layer-id='"+i+"'><svg width='24' height='24' xmlns='http://www.w3.org/2000/svg' fill-rule='evenodd' clip-rule='evenodd'><path d='M12.01 20c-5.065 0-9.586-4.211-12.01-8.424 2.418-4.103 6.943-7.576 12.01-7.576 5.135 0 9.635 3.453 11.999 7.564-2.241 4.43-6.726 8.436-11.999 8.436zm-10.842-8.416c.843 1.331 5.018 7.416 10.842 7.416 6.305 0 10.112-6.103 10.851-7.405-.772-1.198-4.606-6.595-10.851-6.595-6.116 0-10.025 5.355-10.842 6.584zm10.832-4.584c2.76 0 5 2.24 5 5s-2.24 5-5 5-5-2.24-5-5 2.24-5 5-5zm0 1c2.208 0 4 1.792 4 4s-1.792 4-4 4-4-1.792-4-4 1.792-4 4-4z'/></svg></button>";
+            }
+            else{
+                div.innerHTML+="<button class='layerVisibility' layer-id='"+i+"'><svg width='24' height='24' xmlns='http://www.w3.org/2000/svg' fill-rule='evenodd' clip-rule='evenodd'><path d='M8.137 15.147c-.71-.857-1.146-1.947-1.146-3.147 0-2.76 2.241-5 5-5 1.201 0 2.291.435 3.148 1.145l1.897-1.897c-1.441-.738-3.122-1.248-5.035-1.248-6.115 0-10.025 5.355-10.842 6.584.529.834 2.379 3.527 5.113 5.428l1.865-1.865zm6.294-6.294c-.673-.53-1.515-.853-2.44-.853-2.207 0-4 1.792-4 4 0 .923.324 1.765.854 2.439l5.586-5.586zm7.56-6.146l-19.292 19.293-.708-.707 3.548-3.548c-2.298-1.612-4.234-3.885-5.548-6.169 2.418-4.103 6.943-7.576 12.01-7.576 2.065 0 4.021.566 5.782 1.501l3.501-3.501.707.707zm-2.465 3.879l-.734.734c2.236 1.619 3.628 3.604 4.061 4.274-.739 1.303-4.546 7.406-10.852 7.406-1.425 0-2.749-.368-3.951-.938l-.748.748c1.475.742 3.057 1.19 4.699 1.19 5.274 0 9.758-4.006 11.999-8.436-1.087-1.891-2.63-3.637-4.474-4.978zm-3.535 5.414c0-.554-.113-1.082-.317-1.562l.734-.734c.361.69.583 1.464.583 2.296 0 2.759-2.24 5-5 5-.832 0-1.604-.223-2.295-.583l.734-.735c.48.204 1.007.318 1.561.318 2.208 0 4-1.792 4-4z'/></svg></button>";
+            }
+            var bg = document.createElement("img");
+            bg.className="imageBackground";
+            bg.src= "{%static 'assets/icons/transparent.jpg'%}";
+            bg.width=img.width;
+            bg.height=img.height;
+            bg.id="background-img"+i;
+            div.appendChild(img);
+            div.appendChild(bg);
+            div.innerHTML+=oldData;
+            x++;               
+        }
+        for(var x=0;x<this.layers.length;x++){
+            var img=document.getElementById("img"+x);
+            console.log(img);
+            var bg = document.getElementById("background-img"+x);
+            if(bg==null){
+                bg = document.createElement("div");
+                bg.id="background-img"+x;
+            }
+            bg.style.cssText="left:"+0+"; position:'relative';'margin-right':"+0;
+        }
+    }
 }
 function changeHistColor(el){ 
-    console.log(el);
     document.getElementsByClassName("cLBHActive")[0].classList.remove("cLBHActive");
     el.classList.add("cLBHActive");
     canvas.updateHistogram();
+}
+
+function imagedata_to_image(imagedata,i) {
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = imagedata.width;
+    canvas.height = imagedata.height;
+    ctx.putImageData(imagedata, 0, 0);
+
+    var image = new Image();
+    image.src = canvas.toDataURL();
+    image.id="img"+i;
+    image.className="layerImage";
+    return image;
 }
